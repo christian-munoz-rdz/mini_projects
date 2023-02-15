fb056556ad91e8dd15961ee8989cbf9e33042b6a
Christian_Windows_Desktop <chgeovany.15@gmail.com>
2023-02-15 15:28:40 -0600

    Main function added

--- pdfreadear.py
+++ pdfreadear.py
@@ -1,20 +1,29 @@
 import PyPDF2
 
-# abrir el archivo pdf
-with open('Microprocesadores intel - Barry B. Brey.pdf', 'rb') as file:
-    # crear un objeto pdf reader
-    pdf_reader = PyPDF2.PdfFileReader(file)
+def readPdf():
+    # abrir el archivo pdf
+    with open('3-Analisis (Unidad II).pdf', 'rb') as file:
+        # crear un objeto pdf reader
+        pdf_reader = PyPDF2.PdfFileReader(file)
 
-    # obtener el número de páginas
-    num_pages = pdf_reader.numPages
+        # obtener el número de páginas
+        num_pages = pdf_reader.numPages
 
-    # iterar sobre las páginas
-    for page_num in range(52,57):
-        # obtener la página
-        page = pdf_reader.getPage(page_num)
+        # iterar sobre las páginas
+        for page_num in range(num_pages):
+            # obtener la página
+            page = pdf_reader.getPage(page_num)
 
-        # extraer el texto de la página
-        text = page.extractText()
+            # extraer el texto de la página
+            text = page.extractText()
 
-        # imprimir el texto
-        print(text)
+            # imprimir el texto
+            print(text)
+
+def main():
+    readPdf()
+
+if __name__ == '__main__':
+    main()
+
+
